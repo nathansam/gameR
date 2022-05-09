675d53a50318dbfff00f77ce5d81636ae9d5738d
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2022-05-09 13:38:07 +0100

    Fix typos + add spellcheck to testing

--- R/funcs.R
+++ R/funcs.R
@@ -2,7 +2,7 @@
 #' @param palette Character name of palette. Either
 #'   `r paste(names(gameR_colors), sep="", collapse=", ")`
 #' @param reverse Logical. Should the palette be reversed? Defaults to FALSE.
-#' @return Vector containing a hex colour code representation for the chosen
+#' @return Vector containing a hex color code representation for the chosen
 #'   palette
 #' @export
 gameR_cols <- function(palette = NULL, reverse = FALSE) {
@@ -21,12 +21,12 @@ gameR_cols <- function(palette = NULL, reverse = FALSE) {
 #' Generate continuous palette from a discrete gameR palette
 #' @importFrom grDevices colorRampPalette
 #' @inheritParams gameR_cols
-#' @param n Number of colours to be generated
+#' @param n Number of colors to be generated
 #' @param bias Passed to \link{colorRamp}. A positive number. Higher values
 #'   give more widely spaced colors at the high end.
 #' @param interpolate Passed to \link{colorRamp}. Use spline or linear
 #'   interpolation
-#' @return Vector containing a hex colour code representation for the chosen
+#' @return Vector containing a hex color code representation for the chosen
 #'   palette interpolated across \code{n} values
 #' @export
 gameR_cont <- function(n,
